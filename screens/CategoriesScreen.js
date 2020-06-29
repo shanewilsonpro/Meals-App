3fb26ceedc3c4185027ff45be03ee9644c903931
Shane <shanewilsonpro@gmail.com>
2020-06-29 01:23:39 -0700

    creating stacknavigator and navigating screens

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, Button, StyleSheet } from 'react-native';
 
 const CategoriesScreen = props => {
+
     return (
         <View style={styles.screen}>
             <Text>The Categories Screen!</Text>
+            <Button title="Go to Meals!" onPress={() => {
+                props.navigation.navigate("CategoryMeals");
+            }} />
         </View>
     );
 };
