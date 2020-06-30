056b65c44a42af7b6ad08d600c0fe13664bd7835
Shane <shanewilsonpro@gmail.com>
2020-06-29 19:28:20 -0700

    grid of categories and configuring header

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,24 +1,61 @@
-import React from 'react';
-import { Text, View, Button, StyleSheet } from 'react-native';
+import React from "react";
+import {
+  Text,
+  View,
+  FlatList,
+  StyleSheet,
+  TouchableOpacity,
+  Platform
+} from "react-native";
 
-const CategoriesScreen = props => {
+import { CATEGORIES } from "../data/dummy-data";
+import Colors from "../constants/Colors";
 
+const CategoriesScreen = (props) => {
+  const renderGridItem = (itemData) => {
     return (
-        <View style={styles.screen}>
-            <Text>The Categories Screen!</Text>
-            <Button title="Go to Meals!" onPress={() => {
-                props.navigation.navigate("CategoryMeals");
-            }} />
+      <TouchableOpacity
+      style={styles.gridItem}
+        onPress={() => {
+          props.navigation.navigate("CategoryMeals");
+        }}
+      >
+        <View>
+          <Text>{itemData.item.title}</Text>
         </View>
+      </TouchableOpacity>
     );
+  };
+
+  return (
+    <FlatList
+      keyExtractor={(item, index) => item.id}
+      data={CATEGORIES}
+      renderItem={renderGridItem}
+      numColumns={2}
+    />
+  );
+};
+
+CategoriesScreen.navigationOptions = {
+    headerTitle: "Meal Categories",
+    headerStyle: {
+        backgroundColor: Platform.OS === "android" ? Colors.primaryColor : "",
+    },
+    headerTintColor: Platform.OS === "android" ? "white" : Colors.primaryColor,
 };
 
 const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: "center",
-    },
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  gridItem: {
+    flex: 1,
+    margin: 15,
+    height: 150,
+  },
 });
 
 export default CategoriesScreen;
--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, Button, StyleSheet } from 'react-native';
 
 const MealDetailScreen = props => {
     return (
         <View style={styles.screen}>
             <Text>Meal Detail Screen!</Text>
+            <Button title="Go Back to Categories" onPress={() => {
+                props.navigation.popToTop();
+            }}/>
         </View>
     );
 };
